65e33c0b10f1726b9fd7b9720439687f9b0085c1
INT\twlcbo <lobo.chu@gmail.com>
2016-09-26 14:46:08 +0800

    We nolonger need the integerValue field

--- production/source/tw/lobo/math/Fraction.java
+++ production/source/tw/lobo/math/Fraction.java
@@ -4,31 +4,33 @@ package tw.lobo.math;
  * Created by twlcbo on 2016/9/23.
  */
 public class Fraction {
+    private final int numerator;
     private int intgerValue;
     private final int denominator;
 
     public Fraction(int intgerValue) {
         this.intgerValue = intgerValue;
+        this.numerator = intgerValue;
         this.denominator = 1;
     }
 
     public Fraction(int numerator, int denominator) {
         this.intgerValue = numerator;
         this.denominator = denominator;
+        this.numerator = numerator;
     }
 
     public Fraction plus(Fraction that) {
-        this.intgerValue += that.intValue();
-//        return new Fraction(this.intgerValue + that.intValue());
-        return this;
+//        this.intgerValue += that.intValue();
+        return new Fraction(this.numerator+ that.numerator, this.denominator);
     }
 
     public int intValue() {
-        return intgerValue;
+        return this.numerator;
     }
 
     public int getNumerator() {
-        return 3;
+        return this.numerator;
     }
 
     public int getDenominator() {

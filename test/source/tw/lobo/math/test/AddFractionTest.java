a4a38d879e58418d1bd3ff53c288f67921d4e78b
lobochu <lobo.chu@gmail.com>
2016-10-02 17:21:06 +0800

    Wrote a handful more examples for reducing while adding fractions.

--- test/source/tw/lobo/math/test/AddFractionTest.java
+++ test/source/tw/lobo/math/test/AddFractionTest.java
@@ -1,5 +1,6 @@
 package tw.lobo.math.test;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import tw.lobo.math.Fraction;
@@ -41,7 +42,42 @@ public class AddFractionTest {
     }
 
     @Test
+    public void reduceResultToWholeNumber() throws Exception {
+        assertEquals(new Fraction(1), new Fraction(1, 3).plus(new Fraction(2, 3)));
+
+    }
+
+    @Test
     public void differentDenominatorWithoutReducing() throws Exception {
         assertEquals(new Fraction(5, 6), new Fraction(1, 2).plus(new Fraction(1, 3)));
+    }
+
+    @Test
+    public void oneDenominstorIsAMultipleOfTheOther() throws Exception {
+        assertEquals(new Fraction(11, 8), new Fraction(3, 4).plus(new Fraction(5, 8)));
+    }
+
+    @Test
+    public void reduceResultEvenWhenDonominatorAreTheSame() throws Exception {
+        assertEquals(new Fraction(3, 2), new Fraction(3, 4).plus(new Fraction(3, 4)));
+
+    }
+
+    @Test
+    public void commonFactorInDenominators() throws Exception {
+        assertEquals(new Fraction(11, 18), new Fraction(1, 6).plus(new Fraction(4, 9)));
+    }
+
+    @Test
+    public void negativeFractionAndReducing() throws Exception {
+        assertEquals(new Fraction(1, 2), new Fraction(-1, 4).plus(new Fraction(3, 4)));
+        assertEquals(new Fraction(-1, 8), new Fraction(3, 8).plus(new Fraction(-1, 2)));
+    }
+
+    @Test
+    @Ignore("Constructor doesn't yet avoid negative denominators")
+    public void negativeSignsEverywhere() throws Exception {
+        assertEquals(new Fraction(1, 2), new Fraction(1, -4).plus(new Fraction(-3, -4)));
+
     }
 }
